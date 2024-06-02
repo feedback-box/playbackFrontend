ff65c85074cc6d7e111664f19630e52c2a57184f
Nasfame <laciferin@gmail.com>
2024-06-02 06:59:49 +0530

    next14: add api

---
+++ packages/nextjs/pages/api/hive.ts
@@ -0,0 +1,6 @@
+// pages/api/hello.json.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ text: 'Hello' });
+}

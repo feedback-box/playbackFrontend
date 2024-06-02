eb26eee24d387cc75b314281ccc915522a56cd95
Nasfame <laciferin@gmail.com>
2024-06-02 07:53:59 +0530

    hive:

--- packages/nextjs/pages/api/hive.ts
+++ packages/nextjs/pages/api/hive.ts
@@ -35,6 +35,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 }
 
+//TODO: its easy to extract with cat that with open
+
 function extractLocationURL(stdout: string): string | null {
   // Regular expression to find "open: locationURL"
   // const regex = /open\s+(.*)/;
